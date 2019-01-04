23cdea63fce6908f1d9fc49602b84d8efba03daa
Sushant <sushantdhiman@outlook.com>
2019-01-04 20:28:07 +0530

    chore: update dependencies (#7)

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,19 @@ var fs = require('fs');
 var pathModule = require('path');
 
 module.exports = {
-  entry: 'lib/unexpected-date.js',
-  dest: 'unexpected-date.min.js',
-  format: 'umd',
-  moduleName: 'unexpectedDate',
-  sourceMap: true,
-  banner: '/*!\n' + fs.readFileSync(pathModule.resolve(__dirname, 'LICENSE.md'), 'utf-8').replace(/^/mg, ' * ').replace(/\s+$/g, '') + '/\n',
+  input: 'lib/unexpected-date.js',
+  output: {
+    file: 'unexpected-date.min.js',
+    format: 'umd',
+    name: 'unexpectedDate',
+    sourcemap: true,
+    banner: '/*!\n' + fs.readFileSync(pathModule.resolve(__dirname, 'LICENSE.md'), 'utf-8').replace(/^/mg, ' * ').replace(/\s+$/g, '') + '/\n'
+  },
   plugins: [
-    require('rollup-plugin-commonjs')(),
     require('rollup-plugin-node-resolve')(),
     require('rollup-plugin-node-globals')(),
-    require('rollup-plugin-uglify')({
+    require('rollup-plugin-commonjs')(),
+    require('rollup-plugin-uglify').uglify({
       output: {
         comments: function(node, comment) {
           return /^!|@preserve|@license|@cc_on/i.test(comment.value);
